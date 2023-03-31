45b2dd406d7e8155f280656867da7bee0cd5d79c
HongcheonJeong <jhc5317@gmail.com>
2023-03-31 11:24:43 +0900

    Edit CSS little bit

--- js/greetings.js
+++ js/greetings.js
@@ -1,21 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const toDoFormHidden = document.querySelector("form#todo-form");
+const toDoListHidden = document.querySelector("ul#todo-list");
+const quoteDiv = document.querySelector("div#quote");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello ${username}.`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  clock.classList.remove(HIDDEN_CLASSNAME);
+  quoteDiv.classList.remove(HIDDEN_CLASSNAME);
+  toDoFormHidden.classList.remove(HIDDEN_CLASSNAME);
+  toDoListHidden.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
